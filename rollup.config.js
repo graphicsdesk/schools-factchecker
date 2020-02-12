ef6be12eee7a4a92b00cdd8db86151f2568122d3
Jason Kao <jason.kao@columbia.edu>
2020-02-12 16:39:29 -0500

    search functionality

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -46,7 +47,8 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser(),
+    json(),
 	],
 	watch: {
 		clearScreen: false
--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,18 @@
 <script>
-	export let name;
-  import x from '../data/schools_players.json';
+  import DATA from '../data/schools_players.json';
+
+  const schools = Object.keys(DATA);
+  let search = '';
+  $: results = schools.filter(s => search.length > 2 && s.includes(search));
 </script>
 
 <main>
+  <input bind:value={search} placeholder="enter at least 3 letters">
+  <p><b>Search (at least 3 letters): {search}</b></p>
+
+  {#each results as result}
+    <p>{result}</p>
+  {/each}
 
 </main>
 
