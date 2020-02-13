64a99b13e3737d67750bb64960f938309667904b
Jason Kao <jason.kao@columbia.edu>
2020-02-13 14:10:28 -0500

    regex searching

--- src/App.svelte
+++ src/App.svelte
@@ -2,18 +2,30 @@
   import DATA from '../data/schools_players.json';
 
   const schools = Object.keys(DATA);
+  const MIN_LETTERS = 3;
+
   let search = '';
-  $: results = schools.filter(s => search.length > 2 && s.includes(search));
+  $: results = schools.filter(s => {
+    if (search.length < MIN_LETTERS)
+      return false;
+    if (search.charAt(0) === '/') {
+      let re;
+      try {
+        return s.match(new RegExp(search.replace('/', '')));
+      } catch (e) {}
+    }
+    return s.includes(search)
+  });
 </script>
 
 <main>
-  <input bind:value={search} placeholder="enter at least 3 letters">
-  <p><b>Search (at least 3 letters): {search}</b></p>
+  <input bind:value={search} placeholder="enter at least {MIN_LETTERS} letters">
+  <p><b>Search (prefix '/' makes a regex pattern): {search}</b></p>
 
+  <i>Results: {results.length}</i>
   {#each results as result}
     <p>{result}</p>
   {/each}
-
 </main>
 
 <style>
